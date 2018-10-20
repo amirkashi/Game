c9273599e5e76af94f92b23d884a37ccd5f3d689
amirkashi <amka6009@colorado.edu>
2018-10-20 16:41:01 -0600

    some minor changes

--- air_plane.py
+++ air_plane.py
@@ -7,7 +7,7 @@ import pygame.time
 import math
 
 
-class AirPlain():
+class AirPlane():
     def __init__(self):
         self.WIDTH = 600
         self.HEIGHT = 800
@@ -16,11 +16,11 @@ class AirPlain():
         self.HELICOPTER_ELAPSE_TIME = 0
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.keys = [False, False]
-        self.initial_air_plain_location = [225, 650]
-        self.air_plain_locations = [0, 0]
+        self.initial_air_plane_location = [225, 650]
+        self.air_plane_locations = [0, 0]
         self.rockets_positions = []
         self.helicopter_draw_position = [[100, 20]]
-        self.air_plain = pygame.image.load("files/airplane.png")
+        self.air_plane = pygame.image.load("files/airplane.png")
         self.sky = pygame.image.load("files/sky.gif")
         self.rocket = pygame.image.load("files/bomb.png")
         self.helicopter_1 = pygame.image.load("files/heli.png")
