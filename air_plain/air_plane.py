faaa47824f6984409233a0d24cb5d7dc753fc7d2
amirkashi <amka6009@colorado.edu>
2018-11-01 00:05:19 -0600

    time problem fixed

--- air_plain/air_plane.py
+++ air_plain/air_plane.py
@@ -12,9 +12,10 @@ class AirPlane():
         self.WIDTH = 600
         self.HEIGHT = 800
         self.START_TIME = time.time()
-        self.TIME_ELAPSE_INCREASE_SPEED = 10
+        self.TIME_ELAPSE_INCREASE_SPEED = 25
         self.MAX_ELAPSE = 10
         self.time_elapse = 0
+        self.time_elapse_set = set()
         self.rocket_velocity = 10
         self.helicopter_velocity = 1
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
@@ -22,7 +23,7 @@ class AirPlane():
         self.air_plane_location_begin = [self.WIDTH//3, self.HEIGHT-150]
         self.rockets_positions = []
         self.helicopter_draw_time = 100
-        self.helicopter_elapse_time = 200
+        self.helicopter_elapse_time = 50
         self.helicopter_draw_position = [[100, 20]]
         self.air_plane = pygame.image.load("files/airplane.png")
         self.sky = pygame.image.load("files/sky.gif")
@@ -78,7 +79,6 @@ class AirPlane():
                 rocket_rectangle.left = rocket_position[1]
                 rocket_rectangle.top = rocket_position[2]
                 if helicopter_rectangle.colliderect(rocket_rectangle):
-                    #self.air_plane_location_during_game[0] += 1
                     self.helicopter_draw_position.pop(index_helicopter)
                     self.rockets_positions.pop(index_rocket)
             index_helicopter += 1
@@ -106,7 +106,6 @@ class AirPlane():
                     exit(0)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_position = pygame.mouse.get_pos()
-                #self.air_plane_location_during_game[1] += 1
                 self.rockets_positions.append([math.atan2(mouse_position[1]-(self.air_plane_location_begin[1]+70), 
                                 mouse_position[0]-(self.air_plane_location_begin[0])), 
                                 self.air_plane_location_begin[0]+70, 
@@ -116,18 +115,22 @@ class AirPlane():
         left_border = -15
         right_border = 135
         if self.keys[0] and left_border < self.air_plane_location_begin[0]:
-            self.air_plane_location_begin[0] -= 2
+            self.air_plane_location_begin[0] -= 3
         elif self.keys[1] and self.air_plane_location_begin[0] < self.WIDTH - right_border:
-            self.air_plane_location_begin[0] += 2
+            self.air_plane_location_begin[0] += 3
     
     def time_progress(self, game_time):
-        return int(game_time - self.START_TIME)
+        time = int(game_time - self.START_TIME) 
+        time_mode = time % self.TIME_ELAPSE_INCREASE_SPEED
+        if time_mode == 0:
+           if time not in self.time_elapse_set:
+                self.time_elapse_set.add(time)
+                self.increase_speed()
     
     def increase_speed(self):
         self.time_elapse += 1
         self.rocket_velocity += 1
-        #self.helicopter_velocity += 1
-        print(self.rocket_velocity)
+        self.helicopter_velocity += 0.3
     
     def start_the_game(self):
         self.welcome_message()
@@ -141,6 +144,7 @@ class AirPlane():
             self.get_kesy_from_player()
             self.move_airplane()
             game_time = time.time()
-            time_progress_secend = self.time_progress(game_time)
-            if self.time_elapse < self.MAX_ELAPSE and time_progress_secend % self.TIME_ELAPSE_INCREASE_SPEED == 0:
-                self.increase_speed()
+            if self.time_elapse < self.MAX_ELAPSE:
+                self.time_progress(game_time)
+            print(self.rocket_velocity, self.time_elapse_set)
+            
